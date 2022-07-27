2925459821b9ed4722a1b23386d0ca7a3c7ebc01
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-07-27 14:47:38 +0200

    Create 136A - Presents.cpp

---
+++ 136A - Presents.cpp
@@ -0,0 +1,35 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+
+ll a[N], b[N] ,q[N], s[N], w[N], xy[N], l, r, v, x, y, j;
+string a1[N], b1[N];
+char arr[N];
+map<ll, ll>mp, mk;
+set<ll>st;
+
+int main()
+{
+    IO
+    int n; cin >> n;
+    fr1(i, n){
+        cin >> a[i];
+        mp[a[i]] = i;
+    }
+    fr1(i, n) cout << mp[i] << " ";
+    return 0;
+}
