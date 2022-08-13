9dc9d0b9ae987d7437447d817f8a05da72e0b644
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-08-13 14:56:41 +0200

    Create 1624A - Plus One on the Subset.cpp

---
+++ 1624A - Plus One on the Subset.cpp
@@ -0,0 +1,36 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<ll, ll>mp;
+ll a[N], b[N];
+
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    while(Tc--){
+        ll n, mn = 1e10, mx = 0; cin >> n;
+        fr1(i, n){
+            cin >> a[i];
+            mn = min(mn, a[i]);
+            mx = max(mx, a[i]);
+        }
+        cout << mx - mn << endll;
+    }
+    return 0;
+}
