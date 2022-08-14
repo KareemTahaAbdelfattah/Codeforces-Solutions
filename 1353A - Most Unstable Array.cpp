a8c04efe20492b1d7dd73b2de1b42478fa879948
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-08-14 12:43:24 +0200

    Create 1353A - Most Unstable Array.cpp

---
+++ 1353A - Most Unstable Array.cpp
@@ -0,0 +1,33 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<ll, ll>mp;
+ll a[N], b[N];
+
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    while(Tc--){
+        ll n, k; cin >> n >> k;
+        if(n == 1) cout << 0 << endll;
+        else if(n == 2) cout << k << endll;
+        else cout << k * 2 << endll;
+    }
+    return 0;
+}
