b7402f7cad41777a7313918e5ed7c60532a7a4b3
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-08-17 16:33:36 +0200

    Create 1296A - Array with Odd Sum.cpp

---
+++ 1296A - Array with Odd Sum.cpp
@@ -0,0 +1,39 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<ll, ll>mp;
+ll a[N], b[N];
+
+int main()
+{
+    IO
+    ll tc; cin >> tc;
+    while(tc--){
+        ll n, odd = 0, even = 0; cin >> n;
+        for(ll i = 1; i <= n; i++){
+            cin >> a[i];
+            if(a[i] % 2 != 0) odd = 1;
+            if(a[i] % 2 == 0) even = 1;
+            a[i] += a[i - 1];
+        }
+        if((a[n] % 2 != 0) || (odd == 1 && even == 1)) cout << "YES" << endll;
+        else cout << "NO" << endll;
+
+    }
+    return 0;
+}
