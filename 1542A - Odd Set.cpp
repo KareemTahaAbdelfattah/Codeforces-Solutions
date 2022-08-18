519c97dd7f2fffcee05c5f5cea046e34aed98a31
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-08-18 12:47:44 +0200

    Create 1542A - Odd Set.cpp

---
+++ 1542A - Odd Set.cpp
@@ -0,0 +1,37 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<char, ll>mp;
+ll a[N], b[N];
+
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    while(Tc--){
+        ll n, odd = 0, even = 0; cin >> n;
+        fr1(i, 2 * n){
+            cin >> a[i];
+            if(a[i] % 2 == 0) even++;
+            else odd++;
+        }
+        if(even == odd) cout << "YES" << endll;
+        else cout << "NO" << endll;
+    }
+    return 0;
+}
