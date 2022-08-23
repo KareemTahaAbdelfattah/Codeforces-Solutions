646384b114191818f7b9605278855673f208136d
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-08-23 16:01:07 +0200

    Create 1688A - Cirno's Perfect Bitmasks Classroom.cpp

---
+++ 1688A - Cirno's Perfect Bitmasks Classroom.cpp
@@ -0,0 +1,52 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+#define LightOn(n, k)       ((n) | (1<<k))
+#define Lightoff(n, k)      ((n) & (~(1<<k)))
+#define IsOn(n, k)          ((n>>k)&1)
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<ll, ll>mp;
+ll a[N], b[N];
+set<ll>s;
+
+// right shift (n) = num / pow(2, n);
+// left shift (n) = num * pow(2, n);
+// sleep(micro second); for slow execution
+
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    for(ll i = 0; i < 32; i++) mp[pow(2, i)]++;
+    while(Tc--){
+        ll x, y, sum = 0; cin >> x;
+        if(x == 1) cout << 3 << endll;
+        else if(mp[x] == 1) cout << x + 1 << endll;
+        else{
+            for(ll i = 31; i >= 0; i--){
+                if(sum + pow(2, i) < x) sum += pow(2, i);
+                else if(sum + pow(2, i) == x){
+                    y = pow(2, i);
+                    break;
+                }
+            }
+            cout << y << endll;
+        }
+    }
+    return 0;
+}
