df000bb9c4935355514b035ed96909733c2c4f69
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-08-30 22:16:21 +0200

    Create  1722A - Spell Check.cpp

---
+++ 1722A - Spell Check.cpp
@@ -0,0 +1,72 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+ 
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+#define LightOn(n, k)       ((n) | (1<<k))
+#define Lightoff(n, k)      ((n) & (~(1<<k)))
+#define IsOn(n, k)          ((n>>k) & 1)
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<ll, ll>mp;
+ll a[N], b[N];
+set<ll>s;
+ 
+string GetBinary(ll n){
+    string ret = "";
+    for(ll i = 31; i >= 0; i--) ret += to_string(IsOn(n, i));
+    return ret;
+}
+// right shift (n) = num / pow(2, n);
+// left shift (n) = num * pow(2, n);
+// sleep(micro second); for slow execution
+ 
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    while(Tc--){
+        mp.clear();
+        ll n, ans = 0; cin >> n;
+        string s; cin >> s;
+        for(ll i = 0; i < s.size(); i++) mp[s[i]]++;
+        for(ll i = 0; i < s.size(); i++){
+            if(mp[s[i]] == 1 && s[i] == 'T'){
+                mp[s[i]] = 0;
+                ans++;
+            }
+            else if(mp[s[i]] == 1 && s[i] == 'i'){
+                mp[s[i]] = 0;
+                ans++;
+            }
+            else if(mp[s[i]] == 1 && s[i] == 'm'){
+                mp[s[i]] = 0;
+                ans++;
+            }
+            else if(mp[s[i]] == 1 && s[i] == 'u'){
+                mp[s[i]] = 0;
+                ans++;
+            }
+            else if(mp[s[i]] == 1 && s[i] == 'r'){
+                mp[s[i]] = 0;
+                ans++;
+            }
+        }
+        if(ans == 5 && n == 5) cout << "YES" << endll;
+        else cout << "NO" << endll;
+ 
+    }
+    return 0;
+}
