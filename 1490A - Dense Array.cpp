a4dc45cb4709e460ff34d014604b46bc4b96dc9b
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-09-01 13:27:56 +0200

    Create 1490A - Dense Array.cpp

---
+++ 1490A - Dense Array.cpp
@@ -0,0 +1,53 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define fr(i, n)            for(ll i = 0; i < n; i++)
+#define fr1(i, n)           for(ll i = 1; i <= n; i++)
+#define asort(a)            sort(a + 1, a + n + 1)
+#define dsort(a)            sort(a + 1, a + n + 1, greater<int>())
+#define LightOn(n, k)       ((n) | (1<<k))
+#define Lightoff(n, k)      ((n) & (~(1<<k)))
+#define IsOn(n, k)          ((n>>k)&1)
+const long long N = 2e5 + 5, MOD = 1e9 + 7, oo = 1e18;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<ll, ll>mp;
+double a[N], b[N];
+set<ll>s;
+
+// right shift (n) = num / pow(2, n);
+// left shift (n) = num * pow(2, n);
+// sleep(micro second); for slow execution
+
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    while(Tc--){
+        ll n, ans = 0; cin >> n;
+        fr1(i, n) cin >> a[i];
+        for(ll i = 1; i < n; i++){
+            if((max(a[i], a[i + 1]) / min(a[i], a[i + 1])) > 2){
+                double num_min = min(a[i], a[i + 1]), num_max = max(a[i], a[i + 1]);
+                while((num_max / num_min) > 2){
+                    num_min *= 2;
+                    ans++;
+                    //cout << num_min << " ";
+                }
+            }
+            else continue;
+        }
+        //cout << endll;
+        cout << ans << endll;
+    }
+    return 0;
+}
