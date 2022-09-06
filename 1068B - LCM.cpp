fc5e6ca7031834a155a8c32d9d4cd1f08cb5bc36
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-09-06 12:02:20 +0200

    Create 1068B - LCM.cpp

---
+++ 1068B - LCM.cpp
@@ -0,0 +1,39 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+//#define LightOn(n, k)       ((n) | (1<<k))
+//#define Lightoff(n, k)      ((n) & (~(1<<k)))
+//#define IsOn(n, k)          ((n>>k)&1)
+const long long N = 2e5 + 5;
+//const double PI = 3.14;
+//const double Prox = 1e-7;
+map<ll, ll>mp1, mp2, mp3;
+ll a[N], b[N];
+set<ll>s;
+
+// right shift (n) = num / pow(2, n);
+// left shift (n) = num * pow(2, n);
+// sleep(micro second); for slow execution
+
+
+int main()
+{
+    IO
+    ll b; cin >> b;
+    for(ll i = 1; i*i <= b; i++){
+        ll num = (b / __gcd(i, b));
+        s.insert(num);
+        s.insert(__gcd(i, b));
+    }
+    cout << s.size();
+    return 0;
+}
