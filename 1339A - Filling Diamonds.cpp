ba0e6175f659319ecaeea63d8f48124f9dfd18ca
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-09-16 14:13:21 +0200

    Create 1339A - Filling Diamonds.cpp

---
+++ 1339A - Filling Diamonds.cpp
@@ -0,0 +1,32 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define LightOn(n, k)       ((n) | (1<<k))
+#define Lightoff(n, k)      ((n) & (~(1<<k)))
+#define IsOn(n, k)          ((n>>k)&1)
+const long long N = 2e5 + 5;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<ll, ll>mp;
+ll a[N];
+set<ll>s;
+
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    while(Tc--){
+        ll n; cin >> n;
+        cout << n << endll;
+    }
+    return 0;
+}
