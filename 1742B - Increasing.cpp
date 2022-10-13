da05124e46f768839bb29763371da7e67021365c
Kareem Taha <82422224+KareemTaha8178@users.noreply.github.com>
2022-10-13 17:29:37 +0200

    Create 1742B - Increasing.cpp

---
+++ 1742B - Increasing.cpp
@@ -0,0 +1,41 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define LightOn(n, k)       ((n) | (1<<k))
+#define Lightoff(n, k)      ((n) & (~(1<<k)))
+#define IsOn(n, k)          ((n>>k)&1)
+const long long N = 1e6 + 5;
+const long long MOD = 1e9 + 7, OO = 1e18;
+const double PI = 3.14;
+map<ll, ll>mp;
+ll a[N], b[N];
+set<ll>se;
+vector<pair<ll, ll>>v;
+
+int main()
+{
+    IO
+    ll Tc; cin >> Tc;
+    while(Tc--){
+        mp.clear();
+        ll n, flag = 0; cin >> n;
+        for(ll i = 0; i < n; i++){
+            cin >> a[i];
+            mp[a[i]]++;
+            if(mp[a[i]] > 1) flag = 1;
+        }
+        if(flag == 1) cout << "NO" << endll;
+        else cout << "YES" << endll;
+    }
+    return 0;
+}
+
