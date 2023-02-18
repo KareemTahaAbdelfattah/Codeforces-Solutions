d545901573ee9c6dbf60677bec4f8c011d3800a1
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2023-02-18 20:14:45 +0200

    Create U - Number of Ways.cpp

---
+++ U - Number of Ways.cpp
@@ -0,0 +1,49 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define LightOn(n, k)       (n | (1<<k))
+#define Lightoff(n, k)      (n & (~(1<<k)))
+#define IsOn(n, k)          ((n>>k)&1)
+const long long N = 2e5 + 5;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<char, ll>mp1, mp2;
+ll a[N], b[N];
+set<ll>s;
+
+ll GCD(ll a, ll b){
+    return (__gcd(a, b));
+}
+
+ll LCM(ll a, ll b){
+    return ((a * b) / (GCD(a, b)));
+}
+// right shift n = num / pow(2, n);
+// left shift n = num * pow(2, n);
+// sleep(micro second); for slow execution
+ll rec(ll f, ll e);
+int main()
+{
+    IO
+    ll f, e; cin >> f >> e;
+    cout << rec(f, e);
+    return 0;
+}
+
+ll rec(ll f, ll e){
+    if(f > e) return 0;
+    if(f == e) return 1;
+    return rec(f + 1, e) + rec(f + 2, e) + rec(f + 3, e);
+}
+
+
+
