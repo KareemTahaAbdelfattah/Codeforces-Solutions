6131d28d2bcc04053ea256e27daca4261d1de03f
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2023-02-18 14:02:55 +0200

    Create L - Summation.cpp

---
+++ L - Summation.cpp
@@ -0,0 +1,49 @@
+/** ========================================**
+ ** @Author: Kareem Taha Abdelfatah
+ ** @Category: Codeforces problems solutions
+ ** @brief: Problem Solving solutions
+/** ========================================**/
+
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+#define IO                  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endll               "\n"
+#define ll                  long long
+#define LightOn(n, k)       (n | (1<<k))
+#define Lightoff(n, k)      (n & (~(1<<k)))
+#define IsOn(n, k)          ((n>>k)&1)
+const long long N = 2e5 + 5;
+const double PI = 3.14;
+const double Prox = 1e-7;
+map<char, ll>mp1, mp2;
+ll a[N], b[N];
+set<ll>s;
+
+ll GCD(ll a, ll b){
+    return (__gcd(a, b));
+}
+
+ll LCM(ll a, ll b){
+    return ((a * b) / (GCD(a, b)));
+}
+// right shift n = num / pow(2, n);
+// left shift n = num * pow(2, n);
+// sleep(micro second); for slow execution
+ll rec(ll a[], ll n);
+int main()
+{
+    IO
+    ll n; cin >> n;
+    for(ll i = 0; i < n; i++) cin >> a[i];
+    cout << rec(a, n - 1);
+    return 0;
+}
+
+ll rec(ll a[], ll n){
+    if(n < 0) return 0;
+    return (a[n] + rec(a, n - 1));
+}
+
+
+
